71bff175572714afbd54d693dc21879395e08882
Fundy <jiaogeo@126.com>
2021-01-24 23:15:09 +0800

    2021/1/24 login.vue

--- src/main.js
+++ src/main.js
@@ -2,7 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
+import './assets/global.css'
+import axios from 'axios'
 
+axios.defaults.baseURL = 'http://127.0.0.1:1234/'
+
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
